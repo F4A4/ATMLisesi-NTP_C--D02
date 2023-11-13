895bc112a52f76296335400d63ffbaa60b0cb6c0
F4A4 <122023066+F4A4@users.noreply.github.com>
2023-11-13 15:35:53 +0300

    Add files via upload

---
+++ tekrar/tekrar/tekrar/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tekrar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Tiklandiginda(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoşgeldiniz " + textBox1.Text +" Efendim ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+
+            txtYeniIsim.Text = "100";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        //Herhangi bir nesnenin Text özelliği (property si) içine string ifade aldığı için
+        //Türkçe karakter gibi özel karakterleri alabilir. Yani istenilen ifade yazılabilir.
+
+        //Herhangi bir nesnenin Name özelliği (property si) kodlama alanında kullanılan
+        //bir ifade olduğu için string olmasına rağmen türkçe karakter ve boşluk gibi özel
+        //karakterlerin kullanılmaması gerekir.
+    }
+}
---
+++ wpf/wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "Hoş Geldiniz " + textBox1.Text;    
+            
+        }
+
+        private void Acilirken(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
